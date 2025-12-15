cc524a51e06baa930e9b2b3a7af77c5cdf448e25
Natalia Morales <nataliamoralesvaron98@gmail.com>
2025-12-15 20:29:03 +0000

    station_dimension dimension table

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -1,9 +1,16 @@
 WITH BIKE as (
+
     select 
-    *
+    distinct 
+    START_STATIO_ID AS station_id,
+    START_STATION_NAME as station_name,
+    START_LAT as station_lat, 
+    START_LNG as start_station_lng
+
     from {{ source('demo','BIKE')}}
-    limit 10 
+    where RIDE_ID !='ride_id'
+    limit 10
 )
 select 
 *
-from BIKE;
+from BIKE
